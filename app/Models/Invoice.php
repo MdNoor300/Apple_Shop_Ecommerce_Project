cff71e3db27c104531ccb959336b14dc866456e2
MdNoor300 <sehzad.noor98@gmail.com>
2024-01-22 17:41:23 +0600

    model create and relation done

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'total',
+        'discount',
+        'vat',
+        'payable',
+        'cus_details',
+        'ship_details',
+        'shipping_method',
+        'tran_id',
+        'delivery_status',
+        'payment_status',
+        'user_id'
+    ];
+
 }
