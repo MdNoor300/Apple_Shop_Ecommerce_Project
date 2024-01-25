c81f8e0cb0ca91f9e977450784ea35610fb00fa0
MdNoor300 <sehzad.noor98@gmail.com>
2024-01-25 12:27:18 +0600

    add some changes in controller

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use Illuminate\Http\JsonResponse;
+use App\Helper\ResponseHelper;
+
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    //
+    public function BrandList(): JsonResponse
+    {
+        $data = Brand::all();
+        return ResponseHelper::Out('success', $data, 200);
+    }
 }
